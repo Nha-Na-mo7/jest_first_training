8344b8d0406d1841e2d01d154d0d6ad37adb26ea
Nha-Na-mo7 <zirconiump42@gmail.com>
2023-12-15 23:10:02 +0900

    feat: [02] add exception test

--- src/02/index.test.ts
+++ src/02/index.test.ts
@@ -14,3 +14,15 @@ describe("引数が0より大きい数値の場合", () => {
     expect(fizzBuzz(17)).toBe(17);
   });
 });
+
+describe("引数が0以下の数値の場合", () => {
+  const errorMessage = "入力値は0より大きい数値で入力してください。";
+  it("引数が 0 の場合、rangeErrorがthrowされる", () => {
+    expect(() => fizzBuzz(0)).toThrow(RangeError);
+    expect(() => fizzBuzz(0)).toThrow(errorMessage);
+  });
+  it("引数が マイナスの場合、rangeErrorがthrowされる", () => {
+    expect(() => fizzBuzz(0)).toThrow(RangeError);
+    expect(() => fizzBuzz(-3)).toThrow(errorMessage);
+  });
+});
