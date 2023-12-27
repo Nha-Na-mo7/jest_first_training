21745dd9b777b238cb2917dfcb9dd04d2e40591f
Nha-Na-mo7 <zirconiump42@gmail.com>
2023-12-27 18:18:33 +0900

    feat:[02][03] change number 02 and 03

--- src/03/index.test.ts
+++ src/03/index.test.ts
@@ -1,24 +1,28 @@
-import { lunarCalender } from ".";
-import type { Month, JapaneseMonthName } from ".";
+import { fizzBuzz } from ".";
 
-const testCaseList: { input: Month; expected: JapaneseMonthName }[] = [
-  { input: 1, expected: "睦月" },
-  { input: 2, expected: "如月" },
-  { input: 3, expected: "弥生" },
-  { input: 4, expected: "卯月" },
-  { input: 5, expected: "皐月" },
-  { input: 6, expected: "水無月" },
-  { input: 7, expected: "文月" },
-  { input: 8, expected: "葉月" },
-  { input: 9, expected: "長月" },
-  { input: 10, expected: "神無月" },
-  { input: 11, expected: "霜月" },
-  { input: 12, expected: "師走" },
-];
+describe("引数が0より大きい数値の場合", () => {
+  it("3 or 5 で割り切れる整数を渡すと、FizzBuzzが出力される", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("3 で割り切れる整数を渡すと、Fizzが出力される", () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+  it("5 で割り切れる整数を渡すと、Buzzが出力される", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+  it("3 or 5 では割り切れない整数を渡すと、渡した値がそのまま出力される", () => {
+    expect(fizzBuzz(17)).toBe(17);
+  });
+});
 
-it.each(testCaseList)(
-  "引数に $input を渡すと、$expected を返す",
-  ({ input, expected }) => {
-    expect(lunarCalender(input)).toBe(expected);
-  }
-);
+describe("引数が0以下の数値の場合", () => {
+  const errorMessage = "入力値は0より大きい数値で入力してください。";
+  it("引数が 0 の場合、rangeErrorがthrowされる", () => {
+    expect(() => fizzBuzz(0)).toThrow(RangeError);
+    expect(() => fizzBuzz(0)).toThrow(errorMessage);
+  });
+  it("引数が マイナスの場合、rangeErrorがthrowされる", () => {
+    expect(() => fizzBuzz(0)).toThrow(RangeError);
+    expect(() => fizzBuzz(-3)).toThrow(errorMessage);
+  });
+});
--- src/03/index.ts
+++ src/03/index.ts
@@ -1,44 +1,26 @@
-export type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-export type JapaneseMonthName =
-  | "睦月"
-  | "如月"
-  | "弥生"
-  | "卯月"
-  | "皐月"
-  | "水無月"
-  | "文月"
-  | "葉月"
-  | "長月"
-  | "神無月"
-  | "霜月"
-  | "師走";
-export type LunarCalender = (month: Month) => JapaneseMonthName;
+type FizzBuzzString = "Fizz" | "Buzz" | "FizzBuzz";
+type FizzBuzz = (value: number) => number | FizzBuzzString;
 
-export const lunarCalender: LunarCalender = (month: Month) => {
-  switch (month) {
-    case 1:
-      return "睦月";
-    case 2:
-      return "如月";
-    case 3:
-      return "弥生";
-    case 4:
-      return "卯月";
-    case 5:
-      return "皐月";
-    case 6:
-      return "水無月";
-    case 7:
-      return "文月";
-    case 8:
-      return "葉月";
-    case 9:
-      return "長月";
-    case 10:
-      return "神無月";
-    case 11:
-      return "霜月";
-    case 12:
-      return "師走";
+const checkRange = (value: number) => {
+  if (value <= 0) {
+    throw new RangeError("入力値は0より大きい数値で入力してください。");
   }
+};
+
+export const fizzBuzz: FizzBuzz = (value) => {
+  checkRange(value);
+
+  if (value % 15 === 0) {
+    return "FizzBuzz";
+  }
+
+  if (value % 3 === 0) {
+    return "Fizz";
+  }
+
+  if (value % 5 === 0) {
+    return "Buzz";
+  }
+
+  return value;
 };
