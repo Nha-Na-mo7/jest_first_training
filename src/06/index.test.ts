dbd22546bbb42f7418a36b90646df571e0c4a626
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-01-18 23:11:10 +0900

    feat:[06] add test pattern1 return Promise

---
+++ src/06/index.test.ts
@@ -0,0 +1,21 @@
+import { fetchUser } from ".";
+import { User } from "./types";
+
+const user1: User = {
+  id: 1,
+  name: "test1",
+  email: "test1@example.com",
+};
+
+describe("Promiseインスタンスを関数の戻り値としてreturnさせ、Promiseが解決するまでテストの判定を待機させる方法", () => {
+  it("引数に渡したidのユーザーが存在する場合、resolveされる", () => {
+    return fetchUser(1).then((user) => {
+      expect(user).toStrictEqual(user1);
+    });
+  });
+  it("引数に渡したidのユーザーが存在しない場合、rejectされる", () => {
+    return fetchUser(99).catch((error) => {
+      expect(error).toBe("error");
+    });
+  });
+});
