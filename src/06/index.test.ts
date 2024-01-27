a083b506c898f8af5783ddc126edaeb85e6abd45
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-01-27 22:32:14 +0900

    feat:[06] add test pattern3, async test function

--- src/06/index.test.ts
+++ src/06/index.test.ts
@@ -28,3 +28,13 @@ describe("resolveマッチャー、rejectマッチャーを使用する方法", 
     return expect(fetchUser(99)).rejects.toBe("error");
   });
 });
+
+describe("テスト関数をasync 関数とし、テスト関数内でPromiseの解決を待つ方法", () => {
+  it("引数に渡したidのユーザーが存在する場合、resolveされる", async () => {
+    await expect(fetchUser(1)).resolves.toStrictEqual(user1);
+  });
+
+  it("引数に渡したidのユーザーが存在しない場合、rejectされる", async () => {
+    await expect(fetchUser(99)).rejects.toBe("error");
+  });
+});
