3eff6db75f7ad4d3592b94fbda94966144180cab
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-02-15 21:55:25 +0900

    feat:[04] add test base

---
+++ src/04/index.test.ts
@@ -0,0 +1,15 @@
+import { createTestObject } from ".";
+
+const testObject1 = createTestObject({
+  name: "User1",
+  prefecture: "東京都",
+  municipalities: "千代田区",
+  age: 16,
+});
+
+const testObject2 = createTestObject({
+  name: "User2",
+  prefecture: "北海道",
+  municipalities: "札幌市",
+});
+
