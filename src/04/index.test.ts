8c25bada9ec0229045ae18f8f875f795525e6079
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-02-15 22:16:44 +0900

    feat:[04] add toMatchObject matcher test

--- src/04/index.test.ts
+++ src/04/index.test.ts
@@ -13,3 +13,14 @@ const testObject2 = createTestObject({
   municipalities: "札幌市",
 });
 
+describe("オブジェクトのテスト", () => {
+  it("オブジェクトが指定のkey-valueを持つこと", () => {
+    expect(testObject1).toMatchObject({ name: "User1", age: 16 });
+    expect(testObject1).toMatchObject({
+      address: {
+        prefecture: "東京都",
+        municipalities: "千代田区",
+      },
+    });
+  });
+}
