e2ddbfaaa0beac29919b5c9ab1e192e90faf933a
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-02-25 22:00:07 +0900

    feat:[05] create test base

---
+++ src/05/index.test.ts
@@ -0,0 +1,16 @@
+type User = {
+  name: string;
+  age: number;
+};
+const fibonacciArray = [1, 1, 2, 3, 5, 8, 13, 21];
+const userArray: User[] = [
+  { name: "user1", age: 17 },
+  { name: "user2", age: 25 },
+  { name: "user3", age: 33 },
+];
+const booleanObject = {
+  truthy: true,
+  falsy: false,
+  nullValue: null,
+  undefinedValue: undefined,
+};
