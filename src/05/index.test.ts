aed6efe183ae51e925bc36f5ee74c7efa6ca64f8
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-03-01 22:39:40 +0900

    feat:[05] add toContain and arrayContaining

--- src/05/index.test.ts
+++ src/05/index.test.ts
@@ -2,8 +2,15 @@ type User = {
   name: string;
   age: number;
 };
-const fibonacciArray = [1, 1, 2, 3, 5, 8, 13, 21];
-const userArray: User[] = [
+const pacificTeams = [
+  "Fighters",
+  "Eagles",
+  "Lions",
+  "Marines",
+  "Buffaloes",
+  "Hawks",
+];
+const users: User[] = [
   { name: "user1", age: 17 },
   { name: "user2", age: 25 },
   { name: "user3", age: 33 },
@@ -14,3 +21,13 @@ const booleanObject = {
   nullValue: null,
   undefinedValue: undefined,
 };
+describe("配列に関するテスト", () => {
+  it("指定した要素が配列内に含まれていること", () => {
+    expect(pacificTeams).toContain("Fighters");
+    expect(pacificTeams).toEqual(expect.arrayContaining(["Eagles", "Lions"]));
+    expect(pacificTeams).not.toContain("Giants");
+    expect(pacificTeams).not.toEqual(
+      expect.arrayContaining(["Marines", "Swallows"])
+    );
+  });
+});
