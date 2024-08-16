a336ffa5cac6a6263ff71c12c70dd5d6b5f6e8a5
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-17 00:42:09 +0900

    feat:[07] create test

---
+++ src/07/index.test.ts
@@ -0,0 +1,13 @@
+import { unitConversion } from ".";
+
+jest.mock("./modules", () => ({
+  ...jest.requireActual("./modules"),
+  conversionToUSD: (amount: number) => `${Math.floor(amount / 150)}$`,
+}));
+
+it("数値を引数に与えると、何万円か、何米ドルに当たるかを換算する", () => {
+  expect(unitConversion(30000)).toMatchObject({
+    yen: "3万円",
+    usd: "200$",
+  });
+});
