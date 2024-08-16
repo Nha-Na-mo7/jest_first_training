8bbc64053829d7b75a421bc63d837f49a4071a46
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-17 00:02:13 +0900

    feat:[07] create index unitConversion

--- src/07/index.ts
+++ src/07/index.ts
@@ -1,17 +1,8 @@
+import { conversionToTenThousandYen, conversionToUSD } from "./modules";
+
 type UnitConversion = (amount: number) => {
   yen: `${number}万円`;
   usd: `${number}$`;
-};
-
-const conversionToTenThousandYen: (amount: number) => `${number}万円` = (
-  amount: number
-) => `${amount % 10000}万円`;
-
-// この関数は実装が遅れていて、まだ完成していないものとする。
-const conversionToUSD = (amount: number) => {
-  // TODO これから実装予定
-  throw new Error("気が向いたら実装");
-  // more JPY to USD code...
 };
 
 export const unitConversion: UnitConversion = (amount) => {
