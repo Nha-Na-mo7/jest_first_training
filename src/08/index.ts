de72cfb39cf70a488668acf18e5df1aadd7ad29a
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-18 19:39:09 +0900

    feat:[08] create base func

---
+++ src/08/index.ts
@@ -0,0 +1,6 @@
+type ExtractNumberAboutMultipleAndStickThree = (max: number) => Number[];
+
+export const extractNumberAboutMultipleAndStickThree: ExtractNumberAboutMultipleAndStickThree =
+  (max) => {
+    return [1, 2, 3];
+  };
