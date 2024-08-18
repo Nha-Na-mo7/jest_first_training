3392511cefbf9c60adf704ac02a8989842c797bc
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-18 19:56:22 +0900

    feat:[08] update multiples 3 or stick 3

--- src/08/index.ts
+++ src/08/index.ts
@@ -1,6 +1,15 @@
-type ExtractNumberAboutMultipleAndStickThree = (max: number) => Number[];
+type ExtractNumberAboutMultipleAndStickThree = (maxNumber: number) => Number[];
 
 export const extractNumberAboutMultipleAndStickThree: ExtractNumberAboutMultipleAndStickThree =
-  (max) => {
-    return [1, 2, 3];
+  (maxNumber) => {
+    if (maxNumber < 2 || maxNumber > 100) {
+      throw new RangeError("2 ~ 100の数値を入れてください。");
+    }
+
+    const numberArray = [...Array(maxNumber).map((_, index) => index + 1)];
+    const multipleAndStickThreeArray = numberArray.filter((value) => {
+      return value.toString().includes("3") || value % 3 === 0;
+    });
+
+    return multipleAndStickThreeArray;
   };
