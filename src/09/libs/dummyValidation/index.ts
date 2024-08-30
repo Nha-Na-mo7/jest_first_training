bc3ec5da70eaeba82b727d2a9341bca8c52395a8
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-30 21:52:23 +0900

    feat[09]: update dummyValidation

--- src/09/libs/dummyValidation/index.ts
+++ src/09/libs/dummyValidation/index.ts
@@ -4,12 +4,19 @@ const StatusCodes = {
   INTERNAL_SERVER_ERROR: 500,
 };
 
-export const dummyValidation = (() => {
-  const { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR } = StatusCodes;
-
+const randomStatusCode = () => {
   const randomValue = Math.floor(Math.random() * 100);
+  const { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR } = StatusCodes;
 
   if (randomValue < 25) return INTERNAL_SERVER_ERROR;
   if (randomValue < 50) return BAD_REQUEST;
   return OK;
+};
+
+export const dummyValidation = (() => {
+  const statusCode = randomStatusCode();
+
+  return {
+    statusCode,
+  };
 })();
