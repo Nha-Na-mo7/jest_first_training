956908875e949a3c86ba2fa20e9e656c3a844794
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-30 21:05:34 +0900

    feat[09]: create dummyValidation

--- src/09/libs/dummyValidation/index.ts
+++ src/09/libs/dummyValidation/index.ts
@@ -1,12 +1,15 @@
-export const dummyValidation = (() => {
-  const StatusCodes = {
-    OK: 200,
-    BAD_REQUEST: 400,
-    INTERNAL_SERVER_ERROR: 500,
-  };
+const StatusCodes = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  INTERNAL_SERVER_ERROR: 500,
+};
 
-  const status = StatusCodes.OK;
-  return {
-    status,
-  };
+export const dummyValidation = (() => {
+  const { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR } = StatusCodes;
+
+  const randomValue = Math.floor(Math.random() * 100);
+
+  if (randomValue < 25) return INTERNAL_SERVER_ERROR;
+  if (randomValue < 50) return BAD_REQUEST;
+  return OK;
 })();
