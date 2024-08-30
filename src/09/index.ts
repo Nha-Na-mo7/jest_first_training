ed72afe6a21738a7a2d8bb5dd0fcb24f2c86b4f9
Nha-Na-mo7 <zirconiump42@gmail.com>
2024-08-30 22:46:37 +0900

    feat[09]: create dummy fetch

--- src/09/index.ts
+++ src/09/index.ts
@@ -1,1 +1,19 @@
-export const fetchAPI = () => {};
+import { dummyValidation } from "./libs/dummyValidation";
+
+export const dummyFetch = () => {
+  const {
+    result: { statusCode, value },
+  } = dummyValidation;
+
+  if (statusCode === 200) {
+    return value;
+  }
+
+  if (statusCode === 400) {
+    return "バリデーションに問題があります。";
+  }
+
+  if (statusCode === 500) {
+    throw new Error("不明なエラーが発生しました。");
+  }
+};
--- src/09/libs/dummyValidation/index.ts
+++ src/09/libs/dummyValidation/index.ts
@@ -1,12 +1,18 @@
-const StatusCodes = {
+type StatusCodes = {
+  OK: 200;
+  BAD_REQUEST: 400;
+  INTERNAL_SERVER_ERROR: 500;
+};
+
+const statusCodes: StatusCodes = {
   OK: 200,
   BAD_REQUEST: 400,
   INTERNAL_SERVER_ERROR: 500,
 };
 
-const randomStatusCode = () => {
+const createStatusCode = () => {
   const randomValue = Math.floor(Math.random() * 100);
-  const { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR } = StatusCodes;
+  const { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR } = statusCodes;
 
   if (randomValue < 25) return INTERNAL_SERVER_ERROR;
   if (randomValue < 50) return BAD_REQUEST;
@@ -14,9 +20,11 @@ const randomStatusCode = () => {
 };
 
 export const dummyValidation = (() => {
-  const statusCode = randomStatusCode();
-
+  const statusCode = createStatusCode();
   return {
-    statusCode,
+    result: {
+      statusCode,
+      value: "it is value!",
+    },
   };
 })();
